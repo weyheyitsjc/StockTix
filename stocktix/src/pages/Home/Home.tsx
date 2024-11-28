5e43b4d007b86105eb1fb7a20328b70c9eb5c383
Jacy <jyu061313@gmail.com>
2024-11-27 23:56:12 -0500

    Fix type errors

--- stocktix/src/pages/Home/Home.tsx
+++ stocktix/src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const Home = (props: { loggedIn: boolean; email: string; setLoggedIn: boolean;}) => {
+const Home = (props: { loggedIn: boolean; email: string; setLoggedIn:  React.Dispatch<React.SetStateAction<boolean>>;}) => {
   const { loggedIn, email } = props;
   const navigate = useNavigate();
 
